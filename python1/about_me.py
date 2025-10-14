e97da2111b78d8244d3d3accc8976fd282592b1a
linxzi <linxzi1312@gmail.com>
2025-10-14 11:15:08 -0400

    used sys.argv

--- python1/about_me.py
+++ python1/about_me.py
@@ -1,3 +1,22 @@
 #!/usr/bin/env python3
+import sys 
 
-print("My name: Zilin")
+name = sys.argv[1]
+color = sys.argv[2]
+activity = sys.argv[3]
+animal = sys.argv[4]
+
+print("My name: ", name, "\nMy favoriate color:", color, "\nMy favorite activity:", activity, "\nMy favoriate animal:", animal)
+
+#print("My name: Zilin")
+
+#favCol = "Blue"
+#print(f"My favoriate color: {favCol}")
+
+#favAct = "Coding"
+#print("My favorite activity:", favAct)
+
+#favAni = "Chicken"
+#print("My favoriate animal:", favAni,"(Gallus gallus)")
+
+
